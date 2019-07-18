806e156171de9e422a28d354e4c0ad019c1d728d
Mohamed Abdeen Mohamed Ahmed <34784505+mohamed0group@users.noreply.github.com>
2019-07-18 06:39:31 +0200

    portfolio uploading

--- cnames_active.js
+++ cnames_active.js
@@ -1056,6 +1056,7 @@ var cnames_active = {
   "mog-script": "mog-script.github.io",
   "mohamad": "imohamad.github.io",
   "mohammad": "ahmady173.github.io/mohammad",
+  "mohamedabdeen": "mohamed0abdeen.github.io/portfolio",
   "mohatt": "mohatt.github.io",
   "mohit": "mohitgarg.github.io",
   "mohitgupta": "mohitgupta8888.github.io/mohitgupta",
